c20abf824007543fadbc534b907468e9dad03049
Valeriu Navitaniuc <valera.navitaniuc@gmail.com>
2021-09-29 02:32:38 -0400

    Initial commit

---
+++ src/main/java/test/valeriun/test/endpoints/PingController.java
@@ -0,0 +1,13 @@
+package test.valeriun.test.endpoints;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PingController {
+
+    @GetMapping(value = "/api/ping", produces ="application/json")
+    @ResponseStatus(HttpStatus.OK)
+    String ping() {
+        return "{\"success\":true}";
+    }
+}
